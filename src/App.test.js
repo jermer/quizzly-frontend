05c16e81f4694eb8fd949ee629edfbbdaa226ac4
Jason Ermer <jasonermer@gmail.com>
2022-11-07 11:39:48 -0800

    updated app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// smoke test
+it('renders without crashing', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
 });
+
+// snapshot test
+// it('matches snapshot', () => {
+//   const { asFragment } = render(
+//     <MemoryRouter>
+//       <App />
+//     </MemoryRouter>
+//   );
+//   expect(asFragment()).toMatchSnapshot();
+// });
