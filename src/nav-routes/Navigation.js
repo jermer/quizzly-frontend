2384666103f91d24aded62674887342e74468218
Jason Ermer <jasonermer@gmail.com>
2022-11-08 13:08:55 -0800

    initial functionality for site navigation and routes

--- src/nav-routes/Navigation.js
+++ src/nav-routes/Navigation.js
@@ -1,8 +1,85 @@
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import UserContext from "../auth/UserContext";
+
+import "./Navigation.css";
 
 const Navigation = ({ logout }) => {
 
+    const { currentUser } = useContext(UserContext);
+
+    function loggedInNav() {
+        return (
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item mr-4">
+                    <NavLink className="nav-link" to="/discover">
+                        Discover
+                    </NavLink>
+                </li>
+                <li className="nav-item mr-4">
+                    <NavLink className="nav-link" to="/library">
+                        Library
+                    </NavLink>
+                </li>
+                <li className="nav-item mr-4">
+                    <NavLink className="nav-link" to="/reports">
+                        Reports
+                    </NavLink>
+                </li>
+
+                <li className="nav-item dropdown mr-4">
+                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i className="fa-solid fa-user"></i>
+                    </a>
+                    <ul className="dropdown-menu">
+                        <li><span className="dropdown-item-text">
+                            {/* {currentUser.username} */}
+                            USERNAME
+                        </span></li>
+                        <li><hr className="dropdown-divider" /></li>
+                        <li className="dropdown-item">
+                            <Link className="nav-link" to="/profile">
+                                Edit Profile
+                            </Link>
+                        </li>
+                        <li className="dropdown-item">
+                            <Link className="nav-link" to="/" onClick={logout}>
+                                <i className="fa-solid fa-right-from-bracket fa-flip-horizontal"></i> Logout
+                            </Link>
+                        </li>
+
+                    </ul>
+                </li>
+
+            </ul>
+        );
+    }
+
+    function loggedOutNav() {
+        return (
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item mr-4">
+                    <NavLink className="nav-link" to="/login">
+                        Login
+                    </NavLink>
+                </li>
+                <li className="nav-item mr-4">
+                    <NavLink className="nav-link" to="/signup">
+                        Sign Up
+                    </NavLink>
+                </li>
+            </ul>
+        );
+    }
+
     return (
-        null
+        <nav className="Navigation navbar navbar-expand-md">
+            <Link className="navbar-brand" to="/">
+                Quizzly
+            </Link>
+            {/* {currentUser ? loggedInNav() : loggedOutNav()} */}
+            {loggedInNav()}
+        </nav>
     );
 }
 
--- src/nav-routes/QuizzlyRoutes.js
+++ src/nav-routes/QuizzlyRoutes.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "../homepage/Homepage";
 import LoginForm from "../auth/LoginForm";
 import SignupForm from "../auth/SignupForm";
+import QuizList from "../quizzes/QuizList";
 
 const QuizzlyRoutes = ({ login, signup }) => {
 
@@ -11,6 +12,11 @@ const QuizzlyRoutes = ({ login, signup }) => {
             <Route path="/" element={<Homepage />} />
             <Route path="login" element={<LoginForm login={login} />} />
             <Route path="signup" element={<SignupForm signup={signup} />} />
+
+            <Route path="/discover" element={<QuizList />} />
+            <Route path="/library" element={<QuizList />} />
+            <Route path="/reports" element={<QuizList />} />
+            <Route path="/profile" element={<QuizList />} />
         </Routes>
     );
 
