f6ed6663cd6893bc33df32ee6d3b54210b6272a8
Jason Ermer <jasonermer@gmail.com>
2022-11-10 09:46:19 -0800

    preparing to implement quiz editor

--- src/nav-routes/QuizzlyRoutes.js
+++ src/nav-routes/QuizzlyRoutes.js
@@ -3,7 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "../homepage/Homepage";
 import LoginForm from "../auth/LoginForm";
 import SignupForm from "../auth/SignupForm";
+
 import QuizList from "../quizzes/QuizList";
+import QuizForm from "../quizzes/QuizForm";
 
 const QuizzlyRoutes = ({ login, signup }) => {
 
@@ -14,9 +16,14 @@ const QuizzlyRoutes = ({ login, signup }) => {
             <Route path="signup" element={<SignupForm signup={signup} />} />
 
             <Route path="/discover" element={<QuizList />} />
+
+            <Route path="/quizzes/new" element={<QuizForm />} />
+            <Route path="/quizzes/:id" element={<QuizForm />} />
+
             <Route path="/library" element={<QuizList />} />
-            <Route path="/reports" element={<QuizList />} />
-            <Route path="/profile" element={<QuizList />} />
+
+            {/* <Route path="/reports" element={<QuizList />} /> */}
+            {/* <Route path="/profile" element={<QuizList />} /> */}
         </Routes>
     );
 
