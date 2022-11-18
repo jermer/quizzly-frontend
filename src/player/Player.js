f56be6f8a3bc4c8308b61812d34f71630ff1cd16
Jason Ermer <jasonermer@gmail.com>
2022-11-18 15:56:48 -0800

    complete basic functionality for quiz player

--- src/player/Player.js
+++ src/player/Player.js
@@ -6,92 +6,89 @@ import QuestionScreen from "./QuestionScreen";
 
 import LoadingSpinner from "../common/LoadingSpinner";
 import IntroScreen from "./IntroScreen";
+import { shuffleArray } from "../helpers/shuffleArray";
 
 const Player = () => {
     const { id } = useParams();
 
+    console.debug("Quiz Player", "id =", id);
+
+    // holds the current quiz and its questions
     const [quiz, setQuiz] = useState(null);
-    const [currentScreen, setCurrentScreen] = useState(null);
 
+    // holds a randomized list of quiz questions
+    const [questions, setQuestions] = useState([]);
+
+    // holds the index of the current question in the shuffled array
+    const [currQuestionIdx, setCurrQuestionIdx] = useState(null);
+
+    // holds the current question object
     const [currQuestion, setCurrQuestion] = useState(null);
-    const [ansArray, setAnsArray] = useState([]);
 
+    // update quiz data in state when id changes
     useEffect(() => {
         async function fetchQuiz() {
             const quiz = await QuizzlyApi.getQuiz(id);
             setQuiz(quiz);
-            setCurrQuestion(
-                quiz.questions[0]
-            );
-            setCurrentScreen(
+
+            // set up and shuffle the quiz questions
+            setQuestions(shuffleArray(quiz.questions));
+        }
+        fetchQuiz();
+
+    }, [id]);
+
+    // Start the quiz
+    function startQuiz(evt) {
+        evt.preventDefault();
+
+        // set question state to the first question
+        setCurrQuestionIdx(0);
+        setCurrQuestion(questions[0]);
+    }
+
+    function nextQuestion() {
+        // increment the question index
+        const nextQuestionIdx = currQuestionIdx + 1;
+
+        // update state variables for the new question
+        setCurrQuestionIdx(nextQuestionIdx);
+        setCurrQuestion(questions[nextQuestionIdx]);
+    }
+
+    // choose which component to render, one of:
+    // - the intro screen (if no questions have been answered)
+    // - the outro screen (if all questions have been answered)
+    // - the current question screen
+    function selectScreen() {
+        if (!currQuestion) {
+            // return the quiz introduction component
+            return (
                 <IntroScreen
                     title={quiz.title}
                     description={quiz.description}
                     startQuiz={startQuiz}
                 />
             )
-        }
-        fetchQuiz();
-    }, [id]);
-
-    useEffect(() => {
-        if (currQuestion) {
-            // when the current question changes, randomize the answer choice array
-            let arr = [
-                currQuestion.rightA,
-                currQuestion.wrongA1,
-                currQuestion.wrongA2,
-                currQuestion.wrongA3,
-            ]
-            shuffleArray(arr);
-            setAnsArray(arr);
-        }
-    }, [currQuestion])
-
-    // Start the quiz
-    function startQuiz() {
-        console.log("START!");
-        console.log(quiz);
-        setCurrQuestion(
-            quiz.questions[0]
-        );
-        setCurrentScreen(
-            <QuestionScreen
-                qText={currQuestion.qText}
-                aArray={ansArray}
-                handleClick={checkAnswer}
-            />
-        );
-    }
-
-    // Shuffle array
-    // Source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
-    function checkAnswer(evt) {
-        evt.preventDefault();
-        const id = evt.target.closest('button').id;
-        console.log("Click on button", id);
-
-        if (ansArray[id] === currQuestion.rightA) {
-            console.log("CORRECT!");
         } else {
-            console.log("INCORRECT");
+            // return a question screen
+            // for the current question
+            return (
+                <QuestionScreen
+                    question={currQuestion}
+                    nextQuestion={nextQuestion}
+                />
+            )
         }
     }
 
-    // rendering
+    // render the component
 
     if (!quiz) return <LoadingSpinner />;
 
     return (
         <div>
-            {currentScreen}
+            {selectScreen()}
         </div>
     )
 
--- src/player/QuestionScreen.js
+++ src/player/QuestionScreen.js
@@ -1,45 +1,97 @@
+import { useEffect, useState } from "react";
 
-const QuestionScreen = ({ qText, aArray, handleClick }) => {
+import { shuffleArray } from "../helpers/shuffleArray";
 
+const QuestionScreen = ({ question, nextQuestion }) => {
+
+    // holds a randomized array of answer choices
+    const [answers, setAnswers] = useState([]);
+
+    // holds a flag to indicate whether the user has
+    // selected one of the answer choices yet
+    const [done, setDone] = useState(false);
+
+    useEffect(() => {
+        // set up and shuffle the answer choices
+        setAnswers(
+            shuffleArray([
+                question.rightA,
+                question.wrongA1,
+                question.wrongA2,
+                question.wrongA3
+            ]))
+
+        // this question has not yet been answered
+        setDone(false);
+
+    }, [question]);
+
+    // accept clicks on the answer choices
+    function answerClick(evt) {
+        evt.preventDefault();
+
+        const id = evt.target.closest('button').id;
+        console.log("Click on button", id);
+
+        if (answers[id] === question.rightA) {
+            console.log("CORRECT!");
+        } else {
+            console.log("INCORRECT");
+        }
+        setDone(true);
+    }
+
+    // rendering
     return (
         <div>
-            <h3>{qText}</h3>
+            <div className="question-text-area">
+                <h3>{question.qText}</h3>
+            </div>
 
-            <button
-                id="0"
-                className="btn btn-primary"
-                onClick={handleClick}
-            >
-                <i className="fa-solid fa-square me-2"></i> {aArray[0]}
-            </button>
+            <div className="question-answer-area">
+                <button
+                    id="0"
+                    className="btn btn-primary"
+                    onClick={answerClick}
+                >
+                    <i className="fa-solid fa-square me-2"></i> {answers[0]}
+                </button>
 
-            <button
-                id="1"
-                className="btn btn-warning"
-                onClick={handleClick}
-            >
-                <i className="fa-solid fa-diamond me-2"></i> {aArray[1]}
-            </button>
+                <button
+                    id="1"
+                    className="btn btn-warning"
+                    onClick={answerClick}
+                >
+                    <i className="fa-solid fa-diamond me-2"></i> {answers[1]}
+                </button>
 
-            {aArray[2] &&
                 <button
                     id="2"
                     className="btn btn-success"
-                    onClick={handleClick}
+                    onClick={answerClick}
                 >
-                    <i className="fa-solid fa-play fa-rotate-270 me-2"></i> {aArray[2]}
+                    <i className="fa-solid fa-play fa-rotate-270 me-2"></i> {answers[2]}
                 </button>
-            }
 
-            {aArray[3] &&
                 <button
                     id="3"
                     className="btn btn-danger"
-                    onClick={handleClick}
+                    onClick={answerClick}
                 >
-                    <i className="fa-solid fa-circle me-2"></i> {aArray[3]}
+                    <i className="fa-solid fa-circle me-2"></i> {answers[3]}
                 </button>
-            }
+            </div>
+
+            <div className="question-next-button-area">
+                <button
+                    id="next-btn"
+                    className="btn btn-secondary mt-3"
+                    onClick={nextQuestion}
+                    disabled={!done}
+                >
+                    NEXT QUESTION
+                </button>
+            </div>
 
         </div>
     )
