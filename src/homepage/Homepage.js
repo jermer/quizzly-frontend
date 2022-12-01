dae9d43018da71364a3313e487d0267848970b7c
Jason Ermer <jasonermer@gmail.com>
2022-12-01 15:34:34 -0800

    updated homepage about blurb links

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -20,11 +20,11 @@ const Homepage = () => {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <p><i>Quizzly</i> was created by <a href="https://www.linkedin.com/in/jasonermer/" target="_blank">Jason Ermer</a>. The documentation and source code are available on <a href="https://github.com/jermer/quizzly" target="_blank">GitHub</a>.</p>
+                            <p><i>Quizzly</i> was created by <a href="https://www.linkedin.com/in/jasonermer/" target="_blank" rel="noreferrer nofollow">Jason Ermer</a>. The documentation and source code are available on <a href="https://github.com/jermer/quizzly" target="_blank" rel="noreferrer nofollow">GitHub</a>.</p>
 
-                            <p>The project was inspired by and emulates <a href="https://kahoot.com/" target="_blank">Kahoot!</a> It was completed as part of the <a href="https://www.springboard.com/" target="_blank">Springboard</a> Software Engineering Career Track.</p>
+                            <p>The project was inspired by and emulates <a href="https://kahoot.com/" target="_blank" rel="noreferrer nofollow">Kahoot!</a> It was completed as part of the <a href="https://www.springboard.com/" target="_blank" rel="noreferrer nofollow">Springboard</a> Software Engineering Career Track.</p>
 
-                            <p>The logo is <a href="https://game-icons.net/1x1/lord-berandas/artificial-intelligence.html" target="_blank">artificial intelligence</a> by Lord Berandas, available from <a href="https://game-icons.net/" target="_blank">game-icons.net</a> and used under a <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank">CC-BY 3.0</a> license.</p>
+                            <p>The logo is <a href="https://game-icons.net/1x1/lord-berandas/artificial-intelligence.html" target="_blank" rel="noreferrer nofollow">artificial intelligence</a> by Lord Berandas, available from <a href="https://game-icons.net/" target="_blank" rel="noreferrer nofollow">game-icons.net</a> and used under a <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noreferrer nofollow">CC-BY 3.0</a> license.</p>
                         </div>
                     </div>
                 </div>
