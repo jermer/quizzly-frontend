5d89f5d0c750b4922edcfe19f50f35a6099d1ddc
Jason Ermer <jasonermer@gmail.com>
2022-12-02 07:09:07 -0800

    updated homepage and about blurb

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -5,6 +5,7 @@ const Homepage = () => {
             <img
                 src="/logo192.png"
                 alt="quizzly logo"
+                width="150px"
                 className="mb-3"
             />
             <h1>QUIZZLY</h1>
@@ -22,7 +23,7 @@ const Homepage = () => {
                         <div class="modal-body">
                             <p><i>Quizzly</i> was created by <a href="https://www.linkedin.com/in/jasonermer/" target="_blank" rel="noreferrer nofollow">Jason Ermer</a>. The documentation and source code are available on <a href="https://github.com/jermer/quizzly" target="_blank" rel="noreferrer nofollow">GitHub</a>.</p>
 
-                            <p>The project was inspired by and emulates <a href="https://kahoot.com/" target="_blank" rel="noreferrer nofollow">Kahoot!</a> It was completed as part of the <a href="https://www.springboard.com/" target="_blank" rel="noreferrer nofollow">Springboard</a> Software Engineering Career Track.</p>
+                            <p>The project was inspired by and emulates <a href="https://kahoot.com/" target="_blank" rel="noreferrer nofollow">Kahoot!</a> and was completed as part of the <a href="https://www.springboard.com/" target="_blank" rel="noreferrer nofollow">Springboard</a> Software Engineering Career Track.</p>
 
                             <p>The logo is <a href="https://game-icons.net/1x1/lord-berandas/artificial-intelligence.html" target="_blank" rel="noreferrer nofollow">artificial intelligence</a> by Lord Berandas, available from <a href="https://game-icons.net/" target="_blank" rel="noreferrer nofollow">game-icons.net</a> and used under a <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noreferrer nofollow">CC-BY 3.0</a> license.</p>
                         </div>
